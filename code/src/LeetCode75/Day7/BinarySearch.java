781623f2509eccf430b5484c6c9ec83975558a86
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-06-27 20:23:08 +0500

    Add LeetCode 75 Day 7 BinarySearch

---
+++ code/src/LeetCode75/Day7/BinarySearch.java
@@ -0,0 +1,30 @@
+package LeetCode75.Day7;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {5};
+        System.out.println(search(nums, 5));
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            int mid = (start + end)/2;
+
+            if (nums[mid] == target){
+                return mid;
+            }
+
+            if (nums[mid] > target){
+                end = mid - 1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+
+        return -1;
+    }
+}
