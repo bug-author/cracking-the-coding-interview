0cc680cda3611e7fecd2a6ae6b3c03648b097fc3
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-06-27 20:33:19 +0500

    Add LeetCode 75 Day 7 FirstBadVersion

--- code/src/LeetCode75/Day7/BinarySearch.java
+++ code/src/LeetCode75/Day7/BinarySearch.java
@@ -10,18 +10,17 @@ public class BinarySearch {
         int start = 0;
         int end = nums.length - 1;
 
-        while (start <= end){
-            int mid = (start + end)/2;
+        while (start <= end) {
+            int mid = (start + end) / 2;
 
-            if (nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
             }
 
-            if (nums[mid] > target){
+            if (nums[mid] > target) {
                 end = mid - 1;
-            }
-            else {
-                start = mid+1;
+            } else {
+                start = mid + 1;
             }
         }
 
