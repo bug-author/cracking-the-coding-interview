5c64ff7d8934f0fae1104aaf3ce0917f954094e6
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-07-23 16:39:46 +0500

    Add Maze problems

---
+++ code/src/Extras/Recursion/Maze/test.java
@@ -0,0 +1,8 @@
+package Extras.Recursion.Maze;
+
+public class test {
+    public static void main(String[] args) {
+        String test = "abc";
+        System.out.println(test.substring(3));
+    }
+}
