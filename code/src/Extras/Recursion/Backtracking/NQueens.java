dc797d101e76c595a94687f9b4064889b2bbc499
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-07-26 14:50:08 +0500

    Add NQueens

---
+++ code/src/Extras/Recursion/Backtracking/NQueens.java
@@ -0,0 +1,72 @@
+package Extras.Recursion.Backtracking;
+
+public class NQueens {
+    public static void main(String[] args) {
+        boolean[][] board = new boolean[4][4];
+
+        System.out.println(queens(board, 0));
+    }
+
+    static int queens(boolean[][] board, int row) {
+        // base condition
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return 1;
+        }
+
+        int count = 0;
+
+        // placing the queen and checking for every row and column
+        for (int col = 0; col < board.length; col++) {
+            // place the queen if safe
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                // backtrack
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+
+    static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+            for (boolean element : row) {
+                if (element) {
+                    System.out.print("Q");
+                } else {
+                    System.out.print("X");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean isSafe(boolean[][] board, int row, int col) {
+        // check above rows for vertical, diagonal left, diagonal right
+        // No Need to check below rows because they are empty at this time
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        // diagonal left
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+
+        // diagonal right
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
